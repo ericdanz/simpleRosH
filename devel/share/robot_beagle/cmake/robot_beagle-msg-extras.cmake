4c25c34a478449b4e46dd4becd335605faa2aeb0
Eric Danz <ericdanziger@gmail.com>
2014-07-06 14:30:47 -0400

    replacing workspace2

--- devel/share/robot_beagle/cmake/robot_beagle-msg-extras.cmake
+++ devel/share/robot_beagle/cmake/robot_beagle-msg-extras.cmake
@@ -1,2 +1,2 @@
-set(robot_beagle_MESSAGE_FILES "/home/ubuntu/GitHub/simpleRosH/src/robot_beagle/msg/Request.msg;/home/ubuntu/GitHub/simpleRosH/src/robot_beagle/msg/BootResponse.msg;/home/ubuntu/GitHub/simpleRosH/src/robot_beagle/msg/Error.msg;/home/ubuntu/GitHub/simpleRosH/src/robot_beagle/msg/Output.msg;/home/ubuntu/GitHub/simpleRosH/src/robot_beagle/msg/Input.msg")
+set(robot_beagle_MESSAGE_FILES "/home/ubeagle/GitHub/simpleRosH/src/robot_beagle/msg/Request.msg;/home/ubeagle/GitHub/simpleRosH/src/robot_beagle/msg/BootResponse.msg;/home/ubeagle/GitHub/simpleRosH/src/robot_beagle/msg/Error.msg;/home/ubeagle/GitHub/simpleRosH/src/robot_beagle/msg/Output.msg;/home/ubeagle/GitHub/simpleRosH/src/robot_beagle/msg/Input.msg")
 set(robot_beagle_SERVICE_FILES "")
